00ea2a856ff2af10b907decffdd60702d326532d
Airton Lima Junior <airtonlimajr@outlook.com>
2023-05-16 23:24:38 -0300

    Deixando a content page dinamica.

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,20 @@
 export const dataFake = [
     {
-        "id": 1,
+        "id": '1',
         "photoCover": "https://images.unsplash.com/photo-1499332309261-096aa86a939d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1623&q=80",
-        "contentTitle": "Conteudo",
+        "contentTitle": "Conteudo teste 1",
         "contentDescription": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis repellat ullam nobis amet. Distinctio, sed quibusdam aut earum omnis minus ad. Illum culpa qui, laudantium debitis beatae modi! Corporis, nostrum."
     },
     {
-        "id": 2,
+        "id": '2',
         "photoCover": "https://images.unsplash.com/photo-1499332309261-096aa86a939d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1623&q=80",
-        "contentTitle": "Conteudo",
+        "contentTitle": "Conteudo teste 2",
+        "contentDescription": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis repellat ullam nobis amet. Distinctio, sed quibusdam aut earum omnis minus ad. Illum culpa qui, laudantium debitis beatae modi! Corporis, nostrum."
+    },
+    {
+        "id": '3',
+        "photoCover": "https://images.unsplash.com/photo-1499332309261-096aa86a939d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1623&q=80",
+        "contentTitle": "Conteudo teste 3",
         "contentDescription": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis repellat ullam nobis amet. Distinctio, sed quibusdam aut earum omnis minus ad. Illum culpa qui, laudantium debitis beatae modi! Corporis, nostrum."
     },
 ];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import{dataFake} from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -8,9 +9,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ContentComponent implements OnInit {
 
-  photoCover: string = "https://images.unsplash.com/photo-1499332309261-096aa86a939d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1623&q=80";
-  contentTitle: string = "Conteudo";
-  contentDescription: string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis repellat ullam nobis amet. Distinctio, sed quibusdam aut earum omnis minus ad. Illum culpa qui, laudantium debitis beatae modi! Corporis, nostrum.";
+  photoCover: string = "";
+  contentTitle: string = "";
+  contentDescription: string = "";
+
+  private id:any = "0"
 
   constructor(
     private route: ActivatedRoute,
@@ -18,8 +21,20 @@ export class ContentComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(value => {
-      console.log(value.get('id'));
+      this.id = value.get('id');
     });
+
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id: any){
+    const result = dataFake.filter(article => article.id === id)[0];
+   
+      this.contentTitle = result.contentTitle;
+      this.contentDescription = result.contentDescription;
+      this.photoCover = result.photoCover;
+
+    
   }
 
 }
